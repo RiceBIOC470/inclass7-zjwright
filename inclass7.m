aa17dd87bb8d5280832b700f1fb17d621c0315f1
Aryeh <warmflasha@gmail.com>
2017-09-19 12:20:46 -0500

    wrote inclass7

---
+++ inclass7.m
@@ -0,0 +1,22 @@
+%Inclass assignment 7. 
+% 1. The gene Cdx2 is a crucial transcription factor involved in number of
+% developmental stages. Use the UCSC genome browser to answer the following questions
+% about it:
+
+% A. What human chromosome is it located on?
+% B. How many exons does it have?
+% C. What is the precise position of its stop codon in the genome?
+% D. Identify at least one difference in sequence between human and mouse
+% CDX2.
+% E. In which human tissues is it expressed most abundantly?
+
+%2. A. Use the unigene database to find the accession number for a genbank
+% entry containing the complete coding sequence of Cdx2. 
+% B. Use MATLAB to read the genbank information corresponding to that
+% accession number. 
+% C. Use the information read in to find the position of the start and stop
+% codon within the sequence. What are the parts of the sequence before the start codon 
+% and after the stop codon?
+% D. Use the protein_id to read the information on the protein. Use the
+% information you read to determine where the homeobox domain of the protein is.
+% Hint: see the field "Features". 
